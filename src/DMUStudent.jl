81bf32a2c70f7f27c467f8ccfe2d1453a32926d3
Zachary Sunberg <zachary.sunberg@colorado.edu>
2021-01-12 16:59:52 -0700

    hash

--- src/DMUStudent.jl
+++ src/DMUStudent.jl
@@ -1,5 +1,7 @@
 module DMUStudent
 
+using Nettle: hexdigest
+
 export
     # HW1,
     # HW2,
@@ -14,5 +16,10 @@ include("HW3.jl")
 # include("HW6.jl")
 
 is_identikey_colorado_email(email) = !isnothing(match(r"^[a-z]{4}\d{4}@colorado.edu$", email))
+
+function encode_score(email, score, key)
+    @assert is_identikey_colorado_email(email)
+    return hexdigest("sha256", email*string(score)*key)
+end
 
 end # module
