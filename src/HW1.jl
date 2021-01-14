b82b77cd85349dd79a49f855e855f52c233b5b18
Zachary Sunberg <zachary.sunberg@colorado.edu>
2021-01-13 21:02:40 -0700

    added HW1.evaluate docstring

--- src/HW1.jl
+++ src/HW1.jl
@@ -15,6 +15,18 @@ version = v"1.0.11"
 
 @binclude(".bin/hw1_4")
 
+"""
+    evaluate(f, [email]; fname="results.json")
+
+Evaluate homework 1 programming assignment.
+
+# Arguments
+- `f::Function`: submitted function
+- `email::String`: email of submitting student - must match gradescope email!
+
+# Keyword Arguments
+- `fname::String`: submission output file name
+"""
 evaluate = @binclude(".bin/hw1_eval")
 
 end
