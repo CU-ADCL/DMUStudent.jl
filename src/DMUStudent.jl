cfae230e3a59db95fb9e1c65aebd1f59e73ca1e1
Zachary Sunberg <zachary.sunberg@colorado.edu>
2023-01-16 16:54:56 -0700

    2023 homework 1!

--- src/DMUStudent.jl
+++ src/DMUStudent.jl
@@ -4,22 +4,22 @@ using Nettle: hexdigest
 
 include("Obfuscatee.jl")
 
-export HW1,
-       HW2,
-       HW3,
-       HW4,
-       HW5,
-       HW6
+export HW1
+       # HW2,
+       # HW3,
+       # HW4,
+       # HW5,
+       # HW6
 
 function hash_score(hw, email, score, key)
     return hexdigest("sha256", string(hw)*email*string(score)*key)
 end
 
 include("HW1.jl")
-include("HW2.jl")
-include("HW3.jl")
-include("HW4.jl")
-include("HW5.jl")
-include("HW6.jl")
+# include("HW2.jl")
+# include("HW3.jl")
+# include("HW4.jl")
+# include("HW5.jl")
+# include("HW6.jl")
 
 end # module
--- test/hw1.jl
+++ test/hw1.jl
@@ -1,34 +1,36 @@
 @testset "HW1" begin
     using DMUStudent.HW1
 
-    f(x) = max(zero(x),x)
-    results = HW1.evaluate(f, "zachary.sunberg@colorado.edu", fname="good_results.json")
-    @test results.score == 1
+    f(a, bs) = nothing
+    @test HW1.evaluate(f).score==0
+    @test HW1.evaluate(f, "zachary.sunberg@colorado.edu", fname="bad_results.json").score==0
+    @test HW1.evaluate(f, "zachary.sunberg@colorado.edu").score==0
 
-    results = HW1.evaluate(f)
-    @test results.score == 1
+    f(a, bs) = a*first(bs)
+    @test HW1.evaluate(f).score==0
+    @test HW1.evaluate(f, "jodo1234@colorado.edu").score==0
 
-    function g(x)
-        if x > 0
-            return 0
-        else
-            x
+    function solution(a, bs)
+        v = a*first(bs)
+        for b in bs
+            v = max.(v, a*b)
         end
+        return v
     end
-    results = HW1.evaluate(g, "test1234@colorado.edu")
-    @test results.score == 0
+    @test HW1.evaluate(solution).score==1
+    @test HW1.evaluate(solution, "jodo1234@colorado.edu").score==1
 
-    function g(x)
-        if x > 0
-            return x
-        else
-            return 0
+    f(a, bs) = convert(Vector{Float64}, solution(a, bs))
+    @test HW1.evaluate(f).score==0.5
+    @test HW1.evaluate(f, "jodo1234@colorado.edu").score==0.5
+
+    function unstable(a, bs)
+        v = floor.(Int, a*first(bs))
+        for b in bs
+            v = max.(v, a*b)
         end
+        return v
     end
-    results = HW1.evaluate(g, "test1234@colorado.edu")
-    @test results.score == 0
-
-    h(x) = max(0, x)
-    results = HW1.evaluate(h, "test1234@colorado.edu")
-    @test results.score == 0
+    @test HW1.evaluate(unstable).score==0.9
+    @test HW1.evaluate(unstable, "jodo1234@colorado.edu").score==0.9
 end
